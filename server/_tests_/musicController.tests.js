b4f2c8cd7719eaa67731e38dd6077eb88c8e5fb2
Ron Hinton <rhinton1@users.noreply.github.com>
2025-07-05 17:33:50 -0400

    add initial jest test files; setup jest configs (#7)

---
+++ server/_tests_/musicController.tests.js
@@ -0,0 +1,8 @@
+// musicController.test.js
+const { uploadMusic, getAllMusicForUser, getPublicMusicByUser } = require('../controllers/musicController');
+
+describe('Testing music functions...', () => {
+    it('', () => {
+
+    });
+});
---
+++ server/_tests_/usrController.tests.js
@@ -0,0 +1,8 @@
+// usrController.test.js
+const { addConnection, getUserSharedData, removeConnection } = require('../controllers/usrController');
+
+describe('Testing user functions...', () => {
+    it('', () => {
+
+    });
+});
