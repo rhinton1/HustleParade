8a940cab70b4a3665363b84c71480673b7e2dca6
Ron Hinton <rhinton1@users.noreply.github.com>
2025-07-20 15:00:17 -0400

    hpAWS_001: created an iac folder and moved client & server folders under iac to prepare for AWS file structure (#16)

---
+++ iac/server/_tests_/groupController.tests.js
@@ -0,0 +1,8 @@
+// groupController.test.js
+const { createGroup, getGroupsForUser, addGroupMember, removeGroupMember } = require('../controllers/groupControllers');
+
+describe('Testing group chat functions...', () => {
+    it('', () => {
+
+    });
+});
--- iac/server/routes/groupRoutes.js
+++ iac/server/routes/groupRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const groupController = require('../controllers/groupController');
+const groupController = require('../controllers/groupControllers');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/', authMiddleware, groupController.createGroup);
